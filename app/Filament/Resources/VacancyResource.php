9acfef31786a6521c9e8cef90dff98714477bb6a
Roltan <KhalikoR@yandex.ru>
2024-05-20 17:51:22 +0500

    форма вакансий

---
+++ app/Filament/Resources/VacancyResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VacancyResource\Pages;
+use App\Filament\Resources\VacancyResource\RelationManagers;
+use App\Models\Vacancy;
+use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VacancyResource extends Resource
+{
+    protected static ?string $model = Vacancy::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('название')
+                    ->required(),
+                TextInput::make('requirements')
+                    ->label('требования')
+                    ->required(),
+                TextInput::make('duties')
+                    ->label('обязанности')
+                    ->required(),
+                TextInput::make('conditions')
+                    ->label('условия')
+                    ->required(),
+                Checkbox::make('vis')
+                    ->label('отображать?')
+                    ->default(true)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('название'),
+                ToggleColumn::make('vis')
+                    ->label('отображать?'),
+                TextColumn::make('requirements')
+                    ->label('обязанности'),
+                TextColumn::make('duties')
+                    ->label('обязанности'),
+                TextColumn::make('conditions')
+                    ->label('условия')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVacancies::route('/'),
+            'create' => Pages\CreateVacancy::route('/create'),
+            'edit' => Pages\EditVacancy::route('/{record}/edit'),
+        ];
+    }
+}
